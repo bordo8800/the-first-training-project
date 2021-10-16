fccfc21a2970f67925aeb3f5b40c41c0610636f5
Bordo <bordoaleksandr8800@gmail.com>
2021-10-16 12:10:57 +0300

    кажется второй коммит

--- first_leson/script.js
+++ first_leson/script.js
@@ -1,18 +1,29 @@
 // money и time
 let money = +prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
-let firstQuesion = prompt("Введите обязательную статью расходов в этом месяце");
-let secondQuesion = +prompt("Во сколько обойдется?");
+
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        [firstQuesion]:[secondQuesion]
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
-    "savings": false
-}
-let pricePerDay = (money - secondQuesion)/30;
-alert("Ваш буджет на один день" + " " + pricePerDay.toFixed(2));
+    savings: false
+};
+
+for (i = 0; i < 2; i++) {
+    let firstQuesion = prompt("Введите обязательную статью расходов в этом месяце");
+    let secondQuesion = +prompt("Во сколько обойдется?");
+
+    if ((typeof(firstQuesion)) === 'string' && (typeof(firstQuesion)) != null && (typeof(secondQuesion)) != null
+    && firstQuesion != '' && secondQuesion != '' && firstQuesion.length < 50) {
+        console.log ("Done");
+    } else{ }
+
+    appData.expenses[firstQuesion] = secondQuesion;
+
+};
+let dailyBudget = (appData.budget / 30).toFixed(2);
+alert("Ваш бюджет на день " + dailyBudget);
+
