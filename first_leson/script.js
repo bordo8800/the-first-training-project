e9a1988c70cdb6a7c7a6ca0de7de71a01d4d29a3
Bordo <bordoaleksandr8800@gmail.com>
2021-10-16 21:11:53 +0300

    третий коммит

--- first_leson/script.js
+++ first_leson/script.js
@@ -12,18 +12,53 @@ let appData = {
     savings: false
 };
 
-for (i = 0; i < 2; i++) {
+// for (i = 0; i < 2; i++) {
+//     let firstQuesion = prompt("Введите обязательную статью расходов в этом месяце");
+//     let secondQuesion = +prompt("Во сколько обойдется?");
+
+
+//     if ((typeof (firstQuesion)) === 'string' && (typeof (firstQuesion)) != null && (typeof (secondQuesion)) != null &&
+//         firstQuesion != '' && secondQuesion != '' && firstQuesion.length < 50) {
+//         console.log("Done");
+
+//     } else {
+//         console.log("Всё пошло не по плану");
+//         i--;
+//         alert("Миша,всё хуйня, давай по-новой!"); //тут нужно как-то зациклить возможно, я пока не умею, нужно разобраться!
+//     }
+
+//     appData.expenses[firstQuesion] = secondQuesion;
+
+// };
+
+// let i = 0;
+// while (i < 2) {
+//     let firstQuesion = prompt("Введите обязательную статью расходов в этом месяце");
+//     let secondQuesion = +prompt("Во сколько обойдется?");
+//     i++;
+
+//     if ((typeof (firstQuesion)) === 'string' && (typeof (firstQuesion)) != null && (typeof (secondQuesion)) != null &&
+//         firstQuesion != '' && secondQuesion != '' && firstQuesion.length < 50) {
+//         console.log("done");
+//     } else {
+//         console.log("problems")
+//     };
+// }
+
+let i = 0;
+do {
     let firstQuesion = prompt("Введите обязательную статью расходов в этом месяце");
     let secondQuesion = +prompt("Во сколько обойдется?");
+    i++;
 
-    if ((typeof(firstQuesion)) === 'string' && (typeof(firstQuesion)) != null && (typeof(secondQuesion)) != null
-    && firstQuesion != '' && secondQuesion != '' && firstQuesion.length < 50) {
-        console.log ("Done");
-    } else{ }
+    if ((typeof (firstQuesion)) === 'string' && (typeof (firstQuesion)) != null && (typeof (secondQuesion)) != null &&
+        firstQuesion != '' && secondQuesion != '' && firstQuesion.length < 50) {
+        console.log("done");
+    } else {
+        console.log("problems")
+    };
+} while (i < 2);
 
-    appData.expenses[firstQuesion] = secondQuesion;
 
-};
 let dailyBudget = (appData.budget / 30);
-alert("Ваш бюджет на день " + dailyBudget.toFixed(2));
-
+alert("Ваш бюджет на день" + " " + dailyBudget.toFixed(2));
